6ee7397c16a6f7fe20626ac37c8972d76c1a270d
yusufjonc07 <ahmedovyusufjon3@gmail.com>
2025-05-26 18:29:08 +0900

    Clean up user settings

--- src/app/routers/user.py
+++ src/app/routers/user.py
@@ -2,26 +2,12 @@
 from app.utils import *
 from app.functions.user import *
 from app.models.user import *
-from app.models.setting import *
 from app.schemas.user import *
 import math
 
 
 user_router = APIRouter(tags=['User Endpoint'])
 
-class MySetting(BaseModel):
-    hour:float
-
-@user_router.put("/setting_update")
-async def update_setting(
-    form_data: MySetting,
-    db:Session = ActiveSession,
-    usr: UserSchema = Depends(get_current_active_user)
-):
-    db.query(Setting).update({Setting.recall_hour: form_data.hour, Setting.upt: True})
-    db.commit()
-    
-    return "success"
 
 
 @user_router.put("/minute_update")
@@ -35,12 +21,6 @@ async def update_minute(
     
     return "success"
 
-@user_router.get("/settings")
-async def get_setting(
-    db:Session = ActiveSession,
-    usr: UserSchema = Depends(get_current_active_user)
-):
-    return db.query(Setting).first()
 
 
 @user_router.get("/users", description="This router returns list of the users using pagination")
@@ -63,12 +43,11 @@ async def get_users_list(
 @user_router.post("/user/create", description="This router is able to add new user and return user id")
 async def create_new_user(
     form_data: NewUser,
+    usr: UserSchema = Depends(get_current_active_user),
     db:Session = ActiveSession,
 ):
     if not usr.role in ['any_role']:
-        res = create_user(form_data, usr, db)
-        if res:
-            return res
+        return create_user(form_data, usr, db)
     else:
         raise HTTPException(status_code=400, detail="Sizga ruxsat berilmagan!")
 
